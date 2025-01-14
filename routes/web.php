d0b4af07ac954b638c0f257272dd6fb32c1e571a
rozin-ballon <rozin.hack@gmail.com>
2025-01-15 00:43:07 +0900

    change: routesを/fibのみに変更

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\fibController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/fib', [fibController::class, 'getFibNum']);
